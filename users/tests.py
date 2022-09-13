3622587c6803faa17d02c5912e9ed44f077c7377
r00 <15057809+User-r00@users.noreply.github.com>
2022-09-13 04:18:10 -0700

    Setup user admin panel

--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
 
 
 class UserTests(TestCase):
@@ -49,3 +51,38 @@ class UserTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+
+class TestUserAdmin(TestCase):
+    """Test user admin."""
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            username='admin',
+            password='adminpassword')
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com', username='user', password='userpassword')
+
+    def test_list_users(self):
+        """Test admin page users list."""
+        url = reverse('admin:users_user_changelist')
+        result = self.client.get(url)
+
+        self.assertContains(result, self.user.username)
+        self.assertContains(result, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test user admin edit page."""
+        url = reverse('admin:users_user_change', args=[self.user.id])
+        result = self.client.get(url)
+
+        self.assertEqual(result.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test user admin edit page."""
+        url = reverse('admin:users_user_add')
+        result = self.client.get(url)
+
+        self.assertEqual(result.status_code, 200)
